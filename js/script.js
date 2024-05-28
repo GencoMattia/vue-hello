bc022cbb7046ad2b2e9591e1c880be05ce6c905a
GencoMattia <gencomattia@hotmail.it>
2024-05-28 12:55:40 +0200

    added img with src base on data key

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            message: 'Hello Vue!',
+            imgSrc: "https://marketplace.canva.com/EAEMDhEGHCE/1/0/1600w/canva-cat-before-coffee-after-coffee-two-photos-and-text-meme-QZiqeDqC-q4.jpg",
         }
     }
 }).mount('#app')
