e2576b15e881026cc811a59b020e3b856513b304
GencoMattia <gencomattia@hotmail.it>
2024-05-28 13:42:45 +0200

    added img folder

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ createApp({
     data() {
         return {
             message: 'Hello Vue!',
-            imgSrc: "https://marketplace.canva.com/EAEMDhEGHCE/1/0/1600w/canva-cat-before-coffee-after-coffee-two-photos-and-text-meme-QZiqeDqC-q4.jpg",
+            imgSrc: "./",
         }
     }
 }).mount('#app')
