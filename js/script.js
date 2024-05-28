ec5faa7800c5cb97df5003b85666f6f7e115c156
GencoMattia <gencomattia@hotmail.it>
2024-05-28 14:25:59 +0200

    testing function to change class to img

--- js/script.js
+++ js/script.js
@@ -4,7 +4,27 @@ createApp({
     data() {
         return {
             message: 'Hello Vue!',
-            imgSrc: "./",
+            imgSrc: "./img/cat-before.jpg",
+            imgSrcTwo: "./img/cat-after.jpg",
+            imgOneDisplay: "",
+            imgTwoDisplay: "",
         }
-    }
+    },
+
+    methods: {
+        changeImg(imgKey) {
+            let imgOne;
+            let imgTwo;
+            if (imgOne === display-block) {
+                imgOne = display-none
+                imgTwo = display-block
+            } else if (imgTwo === display-block) {
+                imgOne = display-block
+                imgTwo = display-none
+            } else {
+                imgOne = display-block
+                imgTwo = display-none
+            }
+            }
+        },
 }).mount('#app')
